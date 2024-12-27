88205f23d98fc4a9aa281ef3ba8585aac9db4013
youngliqui <super.pav-79@yandex.by>
2024-12-27 17:22:02 +0300

    feat: Book storage service created, PostgresDB connected

---
+++ book-storage-service/src/main/java/by/youngliqui/bookstorageservice/BookStorageServiceApplication.java
@@ -0,0 +1,13 @@
+package by.youngliqui.bookstorageservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookStorageServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookStorageServiceApplication.class, args);
+	}
+
+}
