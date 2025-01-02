e4194ef814baa70161366afe0e1e00c87a19ada2
youngliqui <super.pav-79@yandex.by>
2025-01-02 21:27:58 +0300

    feat: Added a postgresql database for storing users

--- auth-service/src/main/resources/db/changelog/migrations/v1.0_create_entity_tables.sql
+++ auth-service/src/main/resources/db/changelog/migrations/v1.0_create_entity_tables.sql
@@ -2,4 +2,16 @@
 
 --changeset youngliqui:1
 
---comment
+--comment creating entity tables
+
+CREATE SEQUENCE users_id_seq
+    START WITH 1
+    INCREMENT BY 1;
+
+CREATE TABLE users
+(
+    id       BIGINT DEFAULT NEXTVAL('users_id_seq') PRIMARY KEY,
+    username VARCHAR(45) NOT NULL UNIQUE,
+    password VARCHAR(64) NOT NULL,
+    role     VARCHAR(20) NOT NULL
+);
---
+++ auth-service/src/main/resources/db/changelog/migrations/v1.1_add_users.sql
@@ -0,0 +1,3 @@
+INSERT INTO users (username, password, role)
+VALUES ('admin', '$2y$10$WxqKyUb3dmL46Qn4T2TKeOLV5L35eXTDKJxQHJY8fZd5hN9YzwCKe', 'ADMIN'),
+       ('user', '$2y$10$yhiIXvHLGC1OvjEE6yJBqOnVRRDU9zqecKKD076aOHEEmJqsDTjEq', 'USER');
