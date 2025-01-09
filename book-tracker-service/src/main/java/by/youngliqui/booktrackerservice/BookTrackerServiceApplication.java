a7ddf1099907720e6988ee9136bf8bdd65b8e00b
youngliqui <super.pav-79@yandex.by>
2025-01-09 19:49:16 +0300

    feat: Book tracker service init

---
+++ book-tracker-service/src/main/java/by/youngliqui/booktrackerservice/BookTrackerServiceApplication.java
@@ -0,0 +1,13 @@
+package by.youngliqui.booktrackerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookTrackerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookTrackerServiceApplication.class, args);
+	}
+
+}
